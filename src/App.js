762646bd1b94af939f4454472614502d96e2d50e
Maksim <eliott_nu@mail.ru>
2023-11-06 13:44:26 +0000

    Importing and Exporting Components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-function Profile() {
-  return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />;
-}
+import Gallery from "./Gallery.js";
+import { Profile } from "./Gallery.js";
 
-export default function Gallery() {
-  return (
-    <section>
-      <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-    </section>
-  );
+export default function App() {
+  // return <Profile />;
+  return <Gallery />;
 }
