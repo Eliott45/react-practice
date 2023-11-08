acb6a95bc9b2ce8296d03854515c27c2e4dbcf81
Maksim <eliott_nu@mail.ru>
2023-11-08 09:07:50 +0000

    Add simple todo list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import Gallery from "./Gallery.js";
-import { Profile } from "./Gallery.js";
+import Gallery, { Profile } from "./Gallery.js";
 
 export default function App() {
-  // return <Profile />;
-  return <Gallery />;
+  return (
+    <div>
+      <h1>App:</h1>
+      <Gallery />
+      <TodoList />
+      {/* <Profile /> */}
+    </div>
+  );
+}
+
+function TodoList() {
+  return (
+    <>
+      <h1>Hedy Lamarr's Todos</h1>
+      <img
+        src="https://i.imgur.com/yXOvdOSs.jpg"
+        alt="Hedy Lamarr"
+        className="photo"
+      />
+      <ul>
+        <li>Invent new traffic lights</li>
+        <li>Rehearse a movie scene</li>
+        <li>Improve the spectrum technology</li>
+      </ul>
+    </>
+  );
 }
